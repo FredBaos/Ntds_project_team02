bdb4c3d9c097abc02eaa375ca32300d87cd94850
Karthigan Sinnathamby <karsin2605@gmail.com>
2019-12-13 13:35:22 +0100

    adapt some functions and created query function in the notebook

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 METHODS = ['Bert - Spectral Clustering', 'Bert - Random Walk', 'Node2Vec']
 TOPK = 5
 SPECTRAL_CLUSTERING_FILENAME = 'spectral_clustering_embeddings.npy'
-BERT_MEAN_FILENAME = 'bert_mean.npy'
+FAST_MEAN_FILENAME = 'fast_dict.pkl'
 NODE2VEC_FILENAME = 'node2vec.model'
 DF_NODE_FILENAME = 'df_node.csv'
 DF_EDGE_FILENAME = 'df_edge.csv'
