2a20992d8b2ca51340a2e663b07291ca8fa81478
Karthigan Sinnathamby <karsin2605@gmail.com>
2019-12-14 16:31:18 +0100

    corrected spectral clustering

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 METHODS = ['FastText - Random Walk', 'Node2Vec', 'Spectral Clustering']
 TOPK = 5
-SPECTRAL_CLUSTERING_FILENAME = 'spectral.npy'
+SPECTRAL_CLUSTERING_FILENAME = 'spectral.pkl'
 FAST_MEAN_FILENAME = 'fast_dict.pkl'
 NODE2VEC_FILENAME = 'node2vec.model'
 DF_NODE_FILENAME = 'df_node.csv'
--- predict.py
+++ predict.py
@@ -12,11 +12,16 @@ from operator import itemgetter
 
 
 class QueryBot:
-    def __init__(self, node2vec_model, proj_spectral, fasttext_vectors, fasttext_dict, df_node):
+    def __init__(self, node2vec_model, proj_spectral, name2idx_adjacency, ordered_nodes, fasttext_vectors, fasttext_dict, df_node):
         self.node2vec = node2vec_model
+        
         self.proj_spectral =  proj_spectral
+        self.name2idx_adjacency = name2idx_adjacency
+        self.ordered_nodes = ordered_nodes
+        
         self.fasttext_vectors = fasttext_vectors
         self.fasttext_dict = fasttext_dict
+        
         self.df_node = df_node
 
     def make_prediction(self, query, method, topk=TOPK):
@@ -25,7 +30,7 @@ class QueryBot:
         elif method == METHODS[1]:
             return query_answers_node2vec(query, self.node2vec, self.df_node, topk)
         elif method == METHODS[2]:
-            return query_answers_spectral(query, self.proj_spectral, self.df_node, topk)
+            return query_answers_spectral(query, self.proj_spectral, self.ordered_nodes, self.df_node, self.name2idx_adjacency, topk)
         else:
             print("Wrong Method")
 
@@ -35,24 +40,33 @@ def compute_similarities_matrix(vector, proj, topk):
     top_k_similarities = similarities[top_k_indexes]
     return dict(zip(top_k_indexes, top_k_similarities))
 
-def query_answers_spectral(query, matrix, df_node, topn=10, return_idx=True):
-    filtered = df_node[df_node.name==query].index.values
-    if len(filtered)==0:
-        return None
+def query_answers_spectral(query, matrix, ordered_nodes, df_node, name2idx_adjacency, topn=10, return_idx=True):
+    splitted_query = query.split(',')
+    filtered_query = []
+    for x in splitted_query:
+        filtered = df_node[df_node.name==x]
+        if len(filtered)!=0:
+            filtered_query.append(x)
     
-    idx = filtered[0]
-    vector = matrix[idx].reshape(1,-1)
-    results = compute_similarities_matrix(vector, matrix, topn)
+    vectors = []
+    for w in filtered_query:
+        idx = name2idx_adjacency[w]
+        vectors.append(matrix[idx])
+    
+    final_embedding = np.mean(vectors,axis=0)
+
+    results = compute_similarities_matrix(final_embedding, matrix, topn)
     
     similarities = {}
     for idx,v in results.items():
-        if not return_idx:
-            name = df_node[df_node.index==idx].name.values[0]
+        if return_idx:
+            name = ordered_nodes[idx]
+            idx = df_node[df_node.name==name].index.values[0]
+            similarities[idx] = v
         else:
-            name = idx
+            name = ordered_nodes[idx]
+            similarities[name] = v
             
-        similarities[name] = v
-
     similarities = dict(sorted(similarities.items(), key = itemgetter(1), reverse = True))
     return similarities
     
@@ -102,8 +116,11 @@ def query_answers_node2vec(query, model, df_node, topk, return_idx=True):
     
 def load_models(folder, spectral_clustering_filename=SPECTRAL_CLUSTERING_FILENAME,fast_mean_filename=FAST_MEAN_FILENAME,node2vec_filename=NODE2VEC_FILENAME,df_node_filename=DF_NODE_FILENAME):
     
-    spectral_clustering_embed = np.load(os.path.join(folder,spectral_clustering_filename))
-    
+    with open(os.path.join(folder,spectral_clustering_filename),'rb') as f:
+        spectral_clustering_embed = pickle.load(f)
+        ordered_nodes = pickle.load(f)
+        name2idx_adjacency = pickle.load(f)
+            
     node2vec = Word2Vec.load(os.path.join(folder,node2vec_filename))
     
     with open(os.path.join(folder,fast_mean_filename),'rb') as f:
@@ -113,7 +130,7 @@ def load_models(folder, spectral_clustering_filename=SPECTRAL_CLUSTERING_FILENAM
     
     df_node = pd.read_csv(os.path.join(folder,df_node_filename))
 
-    query_bot = QueryBot(node2vec,spectral_clustering_embed,fasttext_vectors,fasttext_dict,df_node)
+    query_bot = QueryBot(node2vec,spectral_clustering_embed,name2idx_adjacency,ordered_nodes,fasttext_vectors,fasttext_dict,df_node)
         
     return query_bot
 
